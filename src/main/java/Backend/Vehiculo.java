1720b869cbc5d5f2184172fc2033fd4a8bf7a4ff
AlexG14303 <adgualotuna1@espe.edu.ec>
2024-02-16 15:29:16 -0500

    datos

---
+++ src/main/java/Backend/Vehiculo.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Backend;
+
+/**
+ *
+ * @author labctr
+ */
+public class Vehiculo {
+    
+    private double tarifa;
+    private double distancia;
+
+    public double getTarifa() {
+        return tarifa;
+    }
+
+    public void setTarifa(double tarifa) {
+        this.tarifa = tarifa;
+    }
+
+    public double getDistancia() {
+        return distancia;
+    }
+
+    public void setDistancia(double distancia) {
+        this.distancia = distancia;
+    }
+    
+}
